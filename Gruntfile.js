d16ba84a0c3dc60c8d2e5b18b2400b18c304f204
Jan-Christoph Sievers <jcs@jan-christoph-sievers.de>
2014-07-13 18:05:42 +0200

    Simple switch between dev and production javascript

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,48 @@
 module.exports = function(grunt) {
-
     // 1. All configuration goes here 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-            sass: {
-                dist: {
-                    options: {
-                        style: 'compressed'
-                    },
-                    files: {
-                        'css/build/main.css': 'css/main.sass'
-                    }
-                } 
-            },
-            concat: {   
-                dist: {
-                    src: [
-                        'js/libs/*.js', // All JS in the libs folder
-                        'js/main.js'  // This specific file
-                    ],
-                    dest: 'js/build/production.js',
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/build/main.css': 'css/main.sass'
                 }
-            },
-            uglify: {
-                build: {
-                    src:  'js/build/production.js',
-                    dest: 'js/build/production.min.js'
-                }
-            },
-            watch: {
-                css: {
-                    files: ['css/*.sass'],
-                    tasks: ['sass'],
-                    options: {
-                        spawn: false
-                    }
+            } 
+        },
+        concat: {   
+            dist: {
+                src: [
+                    'js/libs/*.js', // All JS in the libs folder
+                    'js/main.js'  // This specific file
+                ],
+                dest: 'js/build/production.js',
+            }
+        },
+        uglify: {
+            build: {
+                src:  'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+        watch: {
+            css: {
+                files: ['css/*.sass'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
                 }
             }
-
+        }
     });
-
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-
-
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['sass', 'concat', 'uglify']);
     grunt.registerTask('dev', ['watch']);
-
 };
